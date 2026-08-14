4b31143681bb1161c5e86b53f3c54c2ef294b388
notzorexlol <zorexlegit@gmail.com>
2026-08-14 19:34:57 +0300

    add github social

--- lib/layout.shared.jsx
+++ lib/layout.shared.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaGithub } from "react-icons/fa";
 
 const SUPPORT_SERVER_URL = "https://discord.com/invite/UePr2KBcFY";
+const GITHUB_REPO_URL = "https://github.com/officialboredbot/docs";
 
 export function baseOptions() {
   return {
@@ -14,6 +15,15 @@ export function baseOptions() {
         active: "none",
         external: true,
         icon: <FaDiscord className="size-4" />
+      },
+      {
+        type: "icon",
+        url: GITHUB_REPO_URL,
+        text: "GitHub",
+        label: "GitHub",
+        active: "none",
+        external: true,
+        icon: <FaGithub className="size-4" />
       }
     ],
     nav: {
